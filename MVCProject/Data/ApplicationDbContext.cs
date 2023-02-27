8630d1fe6e7f123e6330a01d642c0ec2b3382e5f
Moonwar <Moonwar.wardiful@brainstation-23.com>
2023-02-27 12:25:43 +0600

    Add project files.

---
+++ MVCProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleMVCProject.Models;
+
+namespace SimpleMVCProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+
+        }
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ MVCProject/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleMVCProject.Models
+{
+    public class Person
+    {
+        [Key]
+        public Guid Key { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string MaritalStatus { get; set; }
+        [Required]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+    }
+}
